d92a2bcf1468bf85bb5d5a02995e54bc35913717
Guruprasad Belthur <coffeedev1@gitlab.com>
2023-06-03 22:19:01 +0530

    perm changed

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Install docker
-apt-get update
-apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
+sudo apt-get update
+sudo apt-get install -y cloud-utils apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update
-apt-get install -y docker-ce
-usermod -aG docker ubuntu
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ubuntu
