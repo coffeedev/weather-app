09f8f608254083e8a2962abf715bbbc7b5190595
Guruprasad Belthur <coffeedev1@gitlab.com>
2023-06-03 21:57:01 +0530

    changes to build

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Stop and remove both containers and the image
-sudo docker container stop weather-app:latest
-sudo docker container rm weather-app:latest
+sudo docker container stop weather-app
+sudo docker container rm weather-app
 sudo docker image rm weather-app:latest
 
 # Now build a new image and push it
@@ -10,4 +10,4 @@ sudo docker build -t weather-app:latest .
 #sudo docker push geepidev/node_weather
 
 # Let's run the 
-sudo docker run -d -p 80:8090 --name weather weather-app:latest
+sudo docker run -d -p 80:8090 --name weather-app weather-app:latest
