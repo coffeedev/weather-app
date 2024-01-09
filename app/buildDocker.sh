572b9c3ec2815fa54c156a6c45c6b54014998ee3
Guruprasad Belthur <coffeedev1@gitlab.com>
2024-01-09 18:35:34 +0530

    buildDocker

--- app/buildDocker.sh
+++ app/buildDocker.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Stop and remove both containers and the image
-NAME=geepidev/weather-app:3.0
-
+DOCKER_TAG=$1
+DOCKER_PAT=$2
+NAME=geepidev/weather-app:${DOCKER_TAG}
 sudo docker container stop ${NAME}
 sudo docker container rm ${NAME}
 sudo docker image rm ${NAME}
 
 # Now build a new image and push it
 sudo docker build -t ${NAME} .
+sudo docker login -u geepidev -p ${DOCKER_PAT}
 sudo docker push ${NAME}
 
 # Let's run the 
